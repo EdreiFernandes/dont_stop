0b45f88b76bfb40e6aef42b20ede11e0b1399f2b
Edrei <edreifernandes@gmail.com>
2021-09-10 01:46:11 -0300

    Corrigindo nomenclatura do pacote letter_selector

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tapple_app/components/LetterSelector/letter_selector.dart';
+import 'package:tapple_app/components/letter_selector/letter_selector.dart';
 
 class Game extends StatelessWidget {
   const Game({Key? key}) : super(key: key);
