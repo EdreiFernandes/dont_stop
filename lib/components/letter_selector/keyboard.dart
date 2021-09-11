515565fd0e12587c848ef13f7b6c9949946db8d1
Edrei <edreifernandes@gmail.com>
2021-09-11 18:35:06 -0300

    Criando arquivo separado para o keyboard_tile

--- lib/components/letter_selector/keyboard.dart
+++ lib/components/letter_selector/keyboard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'keyboard_tile.dart';
+
 class Keyboard extends StatelessWidget {
   final List<String> tiles;
 
@@ -42,51 +44,6 @@ class KeyboardColumn extends StatelessWidget {
           letter: lowerLetter,
         ),
       ],
-    );
-  }
-}
-
-class KeyboardTile extends StatefulWidget {
-  final String letter;
-
-  const KeyboardTile({Key? key, required this.letter}) : super(key: key);
-
-  @override
-  _KeyboardTileState createState() => _KeyboardTileState();
-}
-
-class _KeyboardTileState extends State<KeyboardTile> {
-  late bool _isActive = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: _isActive ? Colors.blue : Colors.grey,
-      textStyle: TextStyle(
-        fontSize: 80.0,
-        color: _isActive ? Colors.white : Colors.white70,
-      ),
-      child: InkWell(
-        onTap: () {
-          setState(() {
-            _isActive = !_isActive;
-          });
-        },
-        child: Container(
-          width: 180.0,
-          height: 120.0,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.white,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              widget.letter,
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
