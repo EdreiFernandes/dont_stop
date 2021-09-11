801b3bfe731bbaff7a3384d8cc93512d094bb951
Edrei <edreifernandes@gmail.com>
2021-09-11 18:30:45 -0300

    Ativando e desativando as letras ao clicar

--- lib/components/letter_selector/keyboard.dart
+++ lib/components/letter_selector/keyboard.dart
@@ -46,34 +46,46 @@ class KeyboardColumn extends StatelessWidget {
   }
 }
 
-class KeyboardTile extends StatelessWidget {
+class KeyboardTile extends StatefulWidget {
   final String letter;
 
   const KeyboardTile({Key? key, required this.letter}) : super(key: key);
 
   @override
+  _KeyboardTileState createState() => _KeyboardTileState();
+}
+
+class _KeyboardTileState extends State<KeyboardTile> {
+  late bool _isActive = true;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 180.0,
-      height: 120.0,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        border: Border.all(
-          color: Colors.white,
-        ),
+    return Material(
+      color: _isActive ? Colors.blue : Colors.grey,
+      textStyle: TextStyle(
+        fontSize: 80.0,
+        color: _isActive ? Colors.white : Colors.white70,
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            letter,
-            style: TextStyle(
-              fontSize: 80.0,
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            _isActive = !_isActive;
+          });
+        },
+        child: Container(
+          width: 180.0,
+          height: 120.0,
+          decoration: BoxDecoration(
+            border: Border.all(
               color: Colors.white,
             ),
           ),
-        ],
+          child: Center(
+            child: Text(
+              widget.letter,
+            ),
+          ),
+        ),
       ),
     );
   }
