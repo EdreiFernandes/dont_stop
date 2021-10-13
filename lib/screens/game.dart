1c8b121c056c8bac7640bd37c6618394d32cee0d
Edrei <edreifernandes@gmail.com>
2021-10-12 21:38:13 -0300

    Criando package components.game

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:tapple_app/components/category_display.dart';
-import 'package:tapple_app/components/letter_selector/letter_selector.dart';
-import 'package:tapple_app/components/progress_bar.dart';
-import 'package:tapple_app/components/timer_button.dart';
+import 'package:tapple_app/components/game/category_display.dart';
+import 'package:tapple_app/components/game/letter_selector/letter_selector.dart';
+import 'package:tapple_app/components/game/progress_bar.dart';
+import 'package:tapple_app/components/game/timer_button.dart';
 
 class Game extends StatelessWidget {
   const Game({Key? key}) : super(key: key);
