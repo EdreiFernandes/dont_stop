0a75011a6e7c7ab4fb8c62ee7daf928e0c98ad15
Edrei <edreifernandes@gmail.com>
2021-10-12 22:16:33 -0300

    Adicionando navigatorDestiny ao MenuButton

---
+++ lib/screens/categories_editor.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoriesEditor extends StatelessWidget {
+  const CategoriesEditor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de categorias"),
+      ),
+      body: Container(
+        child: Text("Olá"),
+      ),
+    );
+  }
+}
