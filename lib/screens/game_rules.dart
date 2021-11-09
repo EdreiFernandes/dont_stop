ffe2057d67206e896c747762c01faeb50c8fc615
Edrei Fernandes <edreifernandes@gmail.com>
2021-11-08 22:21:03 -0300

    Criando base do visualizador de pdf

--- lib/screens/game_rules.dart
+++ lib/screens/game_rules.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path_provider/path_provider.dart';
 
 class GameRules extends StatefulWidget {
   const GameRules({Key? key}) : super(key: key);
@@ -8,7 +13,81 @@ class GameRules extends StatefulWidget {
 }
 
 class _GameRulesState extends State<GameRules> {
-  String pdfPath = "assets/gamerules.pdf";
+  String pdfName = "gamerules.pdf";
+  late String pdfPath;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getFileFromAsset(pdfName).then((file) => {
+          setState(() {
+            pdfPath = file.path;
+            print(pdfPath);
+          })
+        });
+  }
+
+  Future<File> getFileFromAsset(String filename) async {
+    try {
+      var data = await rootBundle.load("assets/$filename");
+      var bytes = data.buffer.asUint8List();
+      var dir = await getApplicationDocumentsDirectory();
+      File file = File("${dir.path}/$filename");
+
+      File assetFile = await file.writeAsBytes(bytes);
+      return assetFile;
+    } catch (e) {
+      throw Exception("Error opening asset files");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("-------"),
+        ),
+        body: Center(
+          child: Builder(
+              builder: (context) => Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        child: Text("Open PDF"),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => PdfViewPage(
+                                        path: pdfPath,
+                                      )));
+                        },
+                      )
+                    ],
+                  )),
+        ),
+      ),
+    );
+  }
+}
+
+class PdfViewPage extends StatefulWidget {
+  final String path;
+
+  const PdfViewPage({Key? key, required this.path}) : super(key: key);
+
+  @override
+  _PdfViewPageState createState() => _PdfViewPageState();
+}
+
+class _PdfViewPageState extends State<PdfViewPage> {
+  int _totalPages = 0;
+  int _currentPage = 0;
+  bool pdfReady = false;
+  late PDFViewController _pdfViewController;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +95,64 @@ class _GameRulesState extends State<GameRules> {
       appBar: AppBar(
         title: Text("Manual de regras"),
       ),
-      body: Text("PDF"),
+      body: Stack(
+        children: [
+          PDFView(
+            filePath: widget.path,
+            autoSpacing: true,
+            enableSwipe: true,
+            pageSnap: true,
+            swipeHorizontal: true,
+            nightMode: false,
+            onError: (e) {
+              print(e);
+            },
+            onRender: (_pages) {
+              setState(() {
+                _totalPages = _pages ?? 0;
+                pdfReady = true;
+              });
+            },
+            onViewCreated: (PDFViewController viewController) {
+              _pdfViewController = viewController;
+            },
+            onPageChanged: (int? page, int? total) {
+              setState(() {});
+            },
+            onPageError: (page, e) {},
+          ),
+          !pdfReady
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Offstage(),
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          _currentPage > 0
+              ? FloatingActionButton.extended(
+                  backgroundColor: Colors.red,
+                  label: Text("Go to ${_currentPage - 1}"),
+                  onPressed: () {
+                    _currentPage -= 1;
+                    _pdfViewController.setPage(_currentPage);
+                  },
+                )
+              : Offstage(),
+          _currentPage + 1 < _totalPages
+              ? FloatingActionButton.extended(
+                  backgroundColor: Colors.green,
+                  label: Text("Go to ${_currentPage + 1}"),
+                  onPressed: () {
+                    _currentPage += 1;
+                    _pdfViewController.setPage(_currentPage);
+                  },
+                )
+              : Offstage(),
+        ],
+      ),
     );
   }
 }
